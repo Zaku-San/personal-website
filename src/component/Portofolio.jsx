06dab1af2d26b7f5420590b4592f442049efa037
Zaku-San <mz8620054@gmail.com>
2024-07-25 21:09:16 +0700

    update porto and icon

--- src/component/Portofolio.jsx
+++ src/component/Portofolio.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import rumah from "../assets/rumah.png";
-import webKelas from "../assets/web-kelas.png"
+import webKelas from "../assets/web-kelas.png";
+import bubble from "../assets/bubble.png"
 import AOS from "aos";
 import "aos/dist/aos.css";
 import Certificate from "./Certificate";
@@ -35,7 +36,7 @@ const Portofolio = () => {
 
         <div className="portofolio flex-col md:flex-row w-full mt-10 flex gap-10   ">
           <div
-            className="box w-full md:w-1/2 shadow-xl hover:scale-90 pb-5 hover:border-2 "
+            className="box w-full md:w-1/3 shadow-xl hover:scale-90 pb-5 hover:border-2 "
             data-aos="flip-down"
             data-aos-duration="1250"
           >
@@ -49,14 +50,11 @@ const Portofolio = () => {
             </a>
             <p className="mx-3">
               Simple slice design with darkmode feature using native javascript{" "}
-              <br />{" "}
-              <span className="text-kuning">
-                note: 
-              </span>
+              <br /> <span className="text-kuning">note:</span>
             </p>
           </div>
           <div
-            className="box w-full md:w-1/2 shadow-xl hover:scale-90 pb-5 hover:border-2 "
+            className="box w-full md:w-1/3 shadow-xl hover:scale-90 pb-5 hover:border-2 "
             data-aos="flip-down"
             data-aos-duration="1250"
           >
@@ -69,11 +67,26 @@ const Portofolio = () => {
               Website Kelas{" "}
             </a>
             <p className="mx-3">
-              Using React, Bootstrap, Swiper js{" "}
-              <br />{" "}
-              <span className="text-kuning">
-              note: 
-              </span>
+              Using React, Bootstrap, Swiper js <br />{" "}
+              <span className="text-kuning">note:</span>
+            </p>
+          </div>
+          <div
+            className="box w-full md:w-1/3 shadow-xl hover:scale-90 pb-5 hover:border-2 "
+            data-aos="flip-down"
+            data-aos-duration="1250"
+          >
+            <img src={bubble} alt="" />
+            <a
+              className="text-lg font-bold text-kuning mx-3 "
+              href="https://bubble-theta.vercel.app/"
+              target="_blank"
+            >
+              Bubble Bash Web Slicing{" "}
+            </a>
+            <p className="mx-3">
+              responsive website using html and css native <br />{" "}
+              <span className="text-kuning">note:</span>
             </p>
           </div>
           <div className="box w-full flex justify-center items-center md:w-1/3 shadow-xl pb-5">
